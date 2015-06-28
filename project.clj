e90500c2312f6a1d8ead190f3a3d492718956f2d
Vladislav Bauer <bauer.vlad@gmail.com>
2015-06-28 18:00:18 +0500

    Update com.taoensso/timbre

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [xmpp-clj "0.3.1"]
                  [jivesoftware/smackx "3.1.0"]
                  [clj-http "1.1.2"]
-                 [com.taoensso/timbre "4.0.1"]
+                 [com.taoensso/timbre "4.0.2"]
                  [sonian/carica "1.1.0" :exclusions [cheshire]]]
 
   :min-lein-version "2.0.0"
