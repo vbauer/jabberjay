6cb7e4ce5dc17aa87076a4fa0513fc7bdf4f8fd7
Vladislav Bauer <bauer.vlad@gmail.com>
2015-07-02 02:36:46 +0500

    Update Clojure to 1.7.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.memoize "0.5.7"]
                  [uochan/watchtower "0.1.4"]
                  [me.raynes/fs "1.4.6"]
